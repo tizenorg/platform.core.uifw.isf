b7fee123a6065308eff846e6da8c96af6fa060d4
Jihoon Kim <jihoon48.kim@samsung.com>
2012-08-30 11:54:44 +0900

    fix indentation
    Change-Id: I170e9e49a04dcf45c732a261d7891bf64c9b87b0

--- ism/demos/isf_prediction_efl.cpp
+++ ism/demos/isf_prediction_efl.cpp
@@ -37,37 +37,37 @@ static Evas_Object *_create_ef_layout (Evas_Object *parent, const char *label, c
 
 static void add_layout_to_conformant (void *data, Evas_Object *lay_in, const char *title)
 {
-   Evas_Object *scroller = NULL;
-   Evas_Object *win = NULL;
-   Evas_Object *conform = NULL;
-   struct appdata *ad = NULL;
+    Evas_Object *scroller = NULL;
+    Evas_Object *win = NULL;
+    Evas_Object *conform = NULL;
+    struct appdata *ad = NULL;
 
-   ad = (struct appdata *) data;
+    ad = (struct appdata *) data;
 
-   win = ad->win_main;
-   // Enabling illume notification property for window
-   elm_win_conformant_set (win, EINA_TRUE);
+    win = ad->win_main;
+    // Enabling illume notification property for window
+    elm_win_conformant_set (win, EINA_TRUE);
 
-   // Creating conformant widget
-   conform = elm_conformant_add (win);
-   evas_object_size_hint_weight_set (conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-   evas_object_show (conform);
+    // Creating conformant widget
+    conform = elm_conformant_add (win);
+    evas_object_size_hint_weight_set (conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_show (conform);
 
-   scroller = elm_scroller_add (ad->naviframe);
+    scroller = elm_scroller_add (ad->naviframe);
 
-   elm_scroller_bounce_set (scroller, EINA_FALSE, EINA_TRUE);
-   evas_object_show (scroller);
+    elm_scroller_bounce_set (scroller, EINA_FALSE, EINA_TRUE);
+    evas_object_show (scroller);
 
-   elm_object_content_set (scroller, lay_in);
-   elm_object_content_set (conform, scroller);
-   elm_naviframe_item_push (ad->naviframe, title, NULL, NULL, conform, NULL);
+    elm_object_content_set (scroller, lay_in);
+    elm_object_content_set (conform, scroller);
+    elm_naviframe_item_push (ad->naviframe, title, NULL, NULL, conform, NULL);
 }
 
 static Evas_Object * create_inner_layout (void *data)
 {
     struct appdata *ad = (struct appdata *)data;
-    Evas_Object *bx = NULL ;
-    Evas_Object *ef = NULL ;
+    Evas_Object *bx = NULL;
+    Evas_Object *ef = NULL;
 
     Evas_Object *parent = ad->naviframe;
 
