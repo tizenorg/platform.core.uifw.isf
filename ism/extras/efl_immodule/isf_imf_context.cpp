06a495cf3c123db04b8fbff4db2d816bcf604c12
Wonkeun Oh <wonkeun.oh@samsung.com>
2015-03-03 10:00:25 +0900

    Sending the H/W key event to helper_launcher for handling highlight ui
    Change-Id: I869d5eb0e18cff8cd84e9a9e38bf4e3d37c2ec8e

--- ism/extras/efl_immodule/isf_imf_context.cpp
+++ ism/extras/efl_immodule/isf_imf_context.cpp
@@ -1573,6 +1573,7 @@ isf_imf_context_focus_in (Ecore_IMF_Context *ctx)
             _panel_client.turn_off (context_scim->id);
         }
 
+        _panel_client.get_active_helper_option (&_active_helper_option);
         _panel_client.send ();
         context_scim->impl->next_shift_status = 0;
         context_scim->impl->shift_mode_enabled = 0;
@@ -2216,9 +2217,18 @@ isf_imf_context_filter_event (Ecore_IMF_Context *ctx, Ecore_IMF_Event_Type type,
         }
         if (!_focused_ic || !_focused_ic->impl || !_focused_ic->impl->is_on) {
             ret = EINA_FALSE;
+#ifdef _TV
+        } else if (_active_helper_option & ISM_HELPER_PROCESS_KEYBOARD_KEYEVENT) {
+            void *pvoid = &ret;
+            _panel_client.process_key_event (key, (int*)pvoid);
+            if (!ret) {
+                ret = _focused_ic->impl->si->process_key_event (key);
+            }
+#else
         } else if (get_keyboard_mode () == TOOLBAR_KEYBOARD_MODE && (_active_helper_option & ISM_HELPER_PROCESS_KEYBOARD_KEYEVENT)) {
             void *pvoid = &ret;
             _panel_client.process_key_event (key, (int*)pvoid);
+#endif
         } else {
             ret = _focused_ic->impl->si->process_key_event (key);
         }
