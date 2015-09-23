1bef3f311fcd6940a5889192562d011dcf5efef3
Sungmin Kwak <sungmin.kwak@samsung.com>
2015-09-23 13:24:27 +0900

    Modify the log dump script file to make isf debug info.
    Change-Id: I170fecb21b6149fc3064082415623608b26eb2e8

--- ism/dump/isf_log_dump.sh
+++ ism/dump/isf_log_dump.sh
@@ -6,9 +6,12 @@
 export DISPLAY=:0.0
 ISF_DEBUG=$1/isf
 ISF_HOME=/home/app/.scim
+ISF_SCIM_HOME=/root/.scim
+ISF_DB=/opt/usr/dbspace
 /bin/mkdir -p ${ISF_DEBUG}
 /bin/cat ${ISF_HOME}/isf.log > ${ISF_DEBUG}/isf.log
 /bin/cat ${ISF_HOME}/config > ${ISF_DEBUG}/config
 /bin/cat ${ISF_HOME}/global > ${ISF_DEBUG}/global
-/bin/cat ${ISF_HOME}/engines_list > ${ISF_DEBUG}/engines_list
+/bin/cat ${ISF_SCIM_HOME}/isf.log > ${ISF_DEBUG}/scim.log
+/bin/cp ${ISF_DB}/.ime_info.db* ${ISF_DEBUG}/
 /bin/sync
