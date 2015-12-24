b06283077b5cf75e9c2d3f2c84dcd0dd67e6f2cc
Jihoon Kim <jihoon48.kim@samsung.com>
2015-12-24 14:02:45 +0900

    Change license in wayland frontend
    Change-Id: If6985bd67187604c741537219f003c024e935d79

--- ism/extras/efl_wsc/isf_wsc_context.h
+++ ism/extras/efl_wsc/isf_wsc_context.h
@@ -1,17 +1,26 @@
 /*
+ * ISF(Input Service Framework)
+ *
+ * ISF is based on SCIM 1.4.7 and extended for supporting more mobile fitable.
  * Copyright © 2013 Intel Corporation
+ * Copyright (c) 2013-2015 Samsung Electronics Co., Ltd.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Contact: Jihoon Kim <jihoon48.kim@samsung.com>, Li Zhang <li2012.zhang@samsung.com>
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc., 51
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
  */
 
 #ifndef __ISF_WSC_CONTEXT_H_
