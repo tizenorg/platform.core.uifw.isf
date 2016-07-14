71b83ba0d99b961c5754e609b3c009933f97173e
InHong Han <inhong1.han@samsung.com>
2016-07-14 16:22:50 +0900

    Fix reset sync issue
    Change-Id: If72ba9c4f9676f15bc68b2c630af686352e92355

--- ism/extras/wayland_immodule/wayland_imcontext.c
+++ ism/extras/wayland_immodule/wayland_imcontext.c
@@ -547,7 +547,7 @@ text_input_commit_string(void                 *data,
     if (!imcontext->ctx)
         return;
 
-#if !(ENABLE_RESET_DONE_FUNC)
+#if ENABLE_RESET_DONE_FUNC
     if (!check_serial(imcontext, serial))
         return;
 #endif
@@ -936,7 +936,7 @@ text_input_preedit_string(void                 *data,
     SECURE_LOGD("preedit event (text: '%s', current pre-edit: '%s')",
                 text,
                 imcontext->preedit_text ? imcontext->preedit_text : "");
-#if !(ENABLE_RESET_DONE_FUNC)
+#if ENABLE_RESET_DONE_FUNC
     if (!check_serial(imcontext, serial))
         return;
 #endif
